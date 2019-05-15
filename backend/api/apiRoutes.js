c3bee68baa68655200749179ffc838080a4febe2
Kevin Nguyen <kevinn858@gmail.com>
2019-05-15 14:15:42 -0700

    added /api test endpoint and started on survey post

--- backend/api/apiRoutes.js
+++ backend/api/apiRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const creationRoutes = require('./creationRoutes.js');
 
+router.get('/', (req, res) => {
+    res.status(200).json({message:"/api endpoint working"});
+});
+
 router.use('/create', creationRoutes);
 
 module.exports = router;
--- backend/api/creationRoutes.js
+++ backend/api/creationRoutes.js
@@ -5,4 +5,10 @@ router.get('/', (req, res) => {
     res.status(201).json({'message':'survey creation routes working'});
 });
 
+router.post('/', (req, res) => {
+    const survey = req.body;
+    const message = "endpoint received survey but survey addition to server not yet supported.";
+    res.status(200).json({message, survey});
+});
+
 module.exports = router;
