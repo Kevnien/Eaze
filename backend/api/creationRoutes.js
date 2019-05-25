5ac393a49b846bc9553597781b8245ace65ea67a
Kevin Nguyen <kevinn858@gmail.com>
2019-05-24 19:20:37 -0700

    updating question now properly changes updated_at field in surveys_table

--- backend/api/creationRoutes.js
+++ backend/api/creationRoutes.js
@@ -4,6 +4,7 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 const db = knex(knexConfig.development);
 
+// READS all the surveys
 router.get('/', (req, res) => {
     db('surveys_table')
         .select()
@@ -20,6 +21,7 @@ router.get('/', (req, res) => {
         });
 });
 
+// CREATES a new survey
 router.post('/', (req, res) => {
     const survey = req.body;
     db('surveys_table')
@@ -30,6 +32,7 @@ router.post('/', (req, res) => {
         .catch(err => res.status(500).json(err.message));
 });
 
+// READS a specific survey by its id number
 router.get('/:id', (req, res) => {
     const {id} = req.params;
     db('surveys_table')
@@ -44,6 +47,7 @@ router.get('/:id', (req, res) => {
         .catch(err => res.status(500).json(err.message));
 });
 
+// UPDATES the name of the survey and updates the 'updated_at' column for the entry
 router.put('/:id', (req, res) => {
     const {id} = req.params;
     const newTitle = req.body;
@@ -81,6 +85,7 @@ router.put('/:id', (req, res) => {
         .catch(err => res.status(500).json({"error":err.message}));
 });
 
+// DELETES a specific survey by its id number
 router.delete('/:id', (req, res) => {
     const {id} = req.params;
     db('surveys_table')
@@ -105,7 +110,7 @@ router.delete('/:id', (req, res) => {
         .catch(err => {res.status(500).json({"server error":err.message})});
 });
 
-// clears a survey of all its questions
+// DELETES all questions for a survey
 router.delete('/delete_questions/:id', (req, res) => {
     const {id} = req.params;
     db('questions_table')
@@ -121,7 +126,7 @@ router.delete('/delete_questions/:id', (req, res) => {
         .catch(err => res.status(500).json({"server error":err.message}));
 });
 
-// creates a new question for a survey
+// CREATES a new question for a survey
 router.post('/:id', (req, res) => {
     const {id} = req.params;
     const question = req.body;
@@ -142,6 +147,66 @@ router.post('/:id', (req, res) => {
             }
         })
         .catch(err => res.status(500).json(err.message));
+});
+
+// READS a question
+router.get('/question/:id', (req, res) => {
+    const {id} = req.params;
+    db('surveys_table')
+        .where({id: id})
+        .then(something => {
+            res.status(200).json({"success":something});
+        })
+        .catch(err => res.status(500).json({"error":err.message}));
+});
+
+// UPDATES a question
+router.put('/question/:id', (req, res) => {
+    const {id} = req.params;
+    const newQuestion = req.body;
+    let oldQuestion = {};
+    let survey_id = 0;
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date+' '+time;
+    db('questions_table')
+        .where('id', '=', id)
+        .then(question => {
+            if(question.length !== 0){
+                oldQuestion = question[0];
+                survey_id = question[0].survey_id;
+                db('surveys_table')
+                    .where({id: survey_id})
+                    .update({updated_at: dateTime})
+                    .then(count => {
+                        if(count === 0){
+                            res.status(400).json({"error":count});
+                        }else{
+                            db('questions_table')
+                                .where({id: id})
+                                .update(newQuestion)
+                                .then(count => {
+                                    if(count === 1){
+                                        res.status(200).json({
+                                            "success":"successfully updated question",
+                                            "id":id,
+                                            "new_question":newQuestion.question,
+                                            "old_question":oldQuestion.question
+                                        });
+                                    }else{
+                                        res.status(400).json({"error":"unable to update question","id":id})
+                                    }
+                                })
+                                .catch(err => res.status(500).json({"error":err.message}));
+                        }
+                    })
+                    .catch(err => res.status(400).json({"error":err.message}));
+            }else{
+                res.status(400).json({"error":"question with that id not found","id":id});
+            }
+        })
+        .catch(err => res.status(500).json({"error":err.message}));
 });
 
 module.exports = router;
